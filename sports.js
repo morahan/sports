fdf3754af9dff014dfe35cf746036b532bbb57f2
Michael Morahan <morahan.mike@gmail.com>
2019-11-24 23:29:38 -0700

    refactoring
    refactored code.  Attempte to create code in one lg object

--- sports.js
+++ sports.js
@@ -1,6 +1,29 @@
 // sports stats and ownership of Win (percentage)
+// V1 -----
+// function Player (firstName, lastName, games, sacks, total){
+//   this.firstName =  firstName;
+//   this.lastName =  lastName;
+//   this.games =  games;
+//   this.sacks =  sacks;
+//   this.total = total;
+//   let self = this;
+// }
+//
+// function winContribution(player){
+//   if (player.total > player.sacks) {
+//     let result = (player.sacks / player.total)
+//     return result;
+//   } else {
+//     return "Total is not greater than sacks";
+//   }
+// }
+// //
+//
+// let mike = new Player("Michael", "Morahan", 1, 2, 3);
+// console.log(winContribution(mike))
+// // ------
 
-function Player (firstName, lastName, games, sacks, total){
+function Player(firstName, lastName, games, sacks, total){
   this.firstName =  firstName;
   this.lastName =  lastName;
   this.games =  games;
@@ -9,15 +32,23 @@ function Player (firstName, lastName, games, sacks, total){
   let self = this;
 }
 
-function winContribution(player){
-  if (player.total > player.sacks) {
-    let result = (player.sacks / player.total)
-    return result;
-  } else {
-    return "Total is not greater than sacks";
-  }
+let team = {
+  player: Player(),
+
+  contribution: function(player){
+    if (player.total > player.sacks) {
+      let result = (player.sacks / player.total)
+      return result;
+    } else {
+      return "Total is not greater than sacks";
+    }
+  },
+
+  playerOne: "First Player"
+
 }
 
 
+
 let mike = new Player("Michael", "Morahan", 1, 2, 3);
-console.log(winContribution(mike))
+console.log(team.player(mike))

5383bf92d9372609737c7956930414cff621a0a0
Michael Morahan <morahan.mike@gmail.com>
2019-11-25 00:57:12 -0700

    Created new Refactor with a few extra K:V pairs

--- sports.js
+++ sports.js
@@ -50,16 +50,32 @@
 // console.log(contribution(mike));
 
 
-// v2.1 ------------
+// v2.2 ------------
+// let self = team;
+
+
 
 let team = {
-  createPlayer: function(firstName, lastName, games, sacks, total){
+  players: [],
+
+  createPlayer: function(firstName, lastName, games, sacks, id){
     this.firstName =  firstName;
     this.lastName =  lastName;
     this.games =  games;
     this.sacks =  sacks;
-    this.total = total;
-    let self = this;
+    this.id = id;
+    let numPlayers = 0;
+    numPlayers ++;
+  },
+
+  totalPlayers: function(){
+    return this.players.length;
+  },
+
+  totalSacks: function() {
+    for (let i = 0; i < this.totalPlayers; i++){
+      let sumSacks = 0;
+    }
   },
 
   contribution: function(individual){
@@ -69,8 +85,14 @@ let team = {
     } else {
       return "Total is not greater than sacks";
     }
-  },
+  }
 }
-let mike = new team.createPlayer("Michael", "Morahan", 1, 2, 3);
+let mike = new team.createPlayer("Michael", "Morahan", 1, 2, 1);
+team.players.push(mike);
 
-console.log(team.contribution(mike));
+console.log(team.totalPlayers())
+
+// let mike = new team.createPlayer("Michael", "Morahan", 1, 2, 1);
+// console.log(team.createPlayer.totalPlayers)
+// console.log(team.contribution(mike));
+// player.i.sacks -->
